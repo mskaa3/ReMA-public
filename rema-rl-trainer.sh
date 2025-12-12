927e231b202e32a3470f8901b431994937beb728
Arkadiusz Janz <arkadiusz.janz@pwr.edu.pl>
2025-12-12 15:27:57 +0100

    new image

--- rema-rl-trainer.sh
+++ rema-rl-trainer.sh
@@ -11,7 +11,8 @@
 cp -r ./data/overall_math $TMPDIR/overall_math
 cp -r ./data/MATH $TMPDIR/MATH
 
-rclone copy s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/verl-rema.sif $TMPDIR/
+# rclone copy s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/verl-rema.sif $TMPDIR/
+rclone copy s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/verl-rema-v2.sif $TMPDIR/
 
 cp ../verl.sif $TMPDIR
 
@@ -23,7 +24,7 @@ COMMAND="unset ROCR_VISIBLE_DEVICES;python3 -m pip install flash-attn==2.7.4.pos
 srun apptainer exec --nv \
     --mount type=bind,src=$TMPDIR,dst=$TMPDIR \
     --mount type=bind,src=$TMPDIR,dst=/root/tmpdir \
-    $TMPDIR/verl-rema.sif \
+    $TMPDIR/verl-rema-v2.sif \
     bash -c "$COMMAND"
 
 if [[ -n $TMPDIR ]]; then
