512f4a952a75874795a94dc14f37f376f456bcf1
Arkadiusz Janz <arkadiusz.janz@pwr.edu.pl>
2025-12-16 16:10:54 +0100

    fixes

--- rema-rl-trainer.sh
+++ rema-rl-trainer.sh
@@ -11,10 +11,8 @@
 cp -r ./data/overall_math $TMPDIR/overall_math
 cp -r ./data/MATH $TMPDIR/MATH
 
-rclone copy s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/verl-rema.sif $TMPDIR/
+# rclone copy s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/verl-rema.sif $TMPDIR/
 rclone copy s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/verl-rema-v2.sif $TMPDIR/
-
-cp ../verl.sif $TMPDIR
 
 source ./env.sh
 export HF_HOME=$TMPDIR/hf_home
