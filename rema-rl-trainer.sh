c5aae34f85be754be341d25d0c43bcc44cb16d67
Arkadiusz Janz <arkadiusz.janz@pwr.edu.pl>
2026-03-27 10:45:15 +0100

    verl copy fix

--- rema-rl-trainer.sh
+++ rema-rl-trainer.sh
@@ -10,18 +10,19 @@
 
 cp -r ./data/overall_math $TMPDIR/overall_math
 cp -r ./data/MATH $TMPDIR/MATH
-cp -r ./src/verl /verl
+cp -r ./src/verl $TMPDIR/verl
 
 rclone copy s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/verl-rema-v3.sif $TMPDIR/
 
 source ./env.sh
 export HF_HOME=$TMPDIR/hf_home
 
-COMMAND="unset ROCR_VISIBLE_DEVICES;python3 -m verl.rema_trainer.main_ppo --config-path=/home/ajanz/projects/ReMA-public/config --config-name=rema-rl.yaml"
+COMMAND="unset ROCR_VISIBLE_DEVICES;export PYTHONPATH=/verl:\$PYTHONPATH;python3 -m verl.rema_trainer.main_ppo --config-path=/home/ajanz/projects/ReMA-public/config --config-name=rema-rl.yaml"
 
 srun apptainer exec --nv --writable-tmpfs \
     --mount type=bind,src=$TMPDIR,dst=$TMPDIR \
     --mount type=bind,src=$TMPDIR,dst=/root/tmpdir \
+    --mount type=bind,src=$TMPDIR/verl,dst=/verl \
     $TMPDIR/verl-rema-v3.sif \
     bash -c "$COMMAND"
 
