d5f6ec2b134e164e4e5fd05da32439916e229902
Arkadiusz Janz <arkadiusz.janz@pwr.edu.pl>
2026-05-13 10:57:45 +0200

    multinode fix

--- rema-rl-trainer-multinode.sh
+++ rema-rl-trainer-multinode.sh
@@ -17,6 +17,7 @@ GPUS_PER_NODE=${GPUS_PER_NODE:-4}
 # This trainer creates two GPU pools, so this is GPUs per node per pool.
 POOL_GPUS_PER_NODE=${POOL_GPUS_PER_NODE:-$((GPUS_PER_NODE / 2))}
 RAY_PORT=${RAY_PORT:-6379}
+RAY_NODE_TMP=${RAY_NODE_TMP:-/tmp/ray-${USER}-${SLURM_JOB_ID}}
 
 SIF_NAME=${SIF_NAME:-verl-rema-v3.sif}
 SIF_REMOTE=${SIF_REMOTE:-s3v2:s3min-tomasznaskret-1712063354/user/dmotyka/sif_images/${SIF_NAME}}
@@ -46,17 +47,25 @@ cp -r ./data/MATH "$JOB_TMP/MATH"
 cp -r ./src/verl "$JOB_TMP/verl"
 rclone copy "$SIF_REMOTE" "$JOB_TMP/"
 
+echo "Preparing node-local Ray temp dir at ${RAY_NODE_TMP}"
+srun --nodes="${SLURM_NNODES}" --ntasks="${SLURM_NNODES}" bash -lc '
+    rm -rf "'"${RAY_NODE_TMP}"'"
+    mkdir -p "'"${RAY_NODE_TMP}"'"
+'
+
 COMMON_MOUNTS=(
     --mount "type=bind,src=${JOB_TMP},dst=${JOB_TMP}"
     --mount "type=bind,src=${JOB_TMP},dst=/root/tmpdir"
     --mount "type=bind,src=${JOB_TMP}/verl,dst=/verl"
     --mount "type=bind,src=${JOB_TMP}/verl,dst=/root/ReMA-public/src/verl"
+    --mount "type=bind,src=${RAY_NODE_TMP},dst=${RAY_NODE_TMP}"
 )
 
 echo "Starting Ray head on ${HEAD_NODE} (${IP_HEAD})"
 srun --nodes=1 --ntasks=1 -w "$HEAD_NODE" \
     apptainer exec --nv --writable-tmpfs "${COMMON_MOUNTS[@]}" "$JOB_TMP/${SIF_NAME}" \
     ray start --head --node-ip-address="$HEAD_NODE_IP" --port="$RAY_PORT" \
+        --temp-dir="$RAY_NODE_TMP" \
         --num-cpus="${SLURM_CPUS_PER_TASK}" --num-gpus="${GPUS_PER_NODE}" --block &
 sleep 15
 
@@ -67,11 +76,13 @@ for ((i = 1; i <= WORKER_COUNT; i++)); do
     srun --nodes=1 --ntasks=1 -w "$NODE" \
         apptainer exec --nv --writable-tmpfs "${COMMON_MOUNTS[@]}" "$JOB_TMP/${SIF_NAME}" \
         ray start --address "$IP_HEAD" \
+            --temp-dir="$RAY_NODE_TMP" \
             --num-cpus="${SLURM_CPUS_PER_TASK}" --num-gpus="${GPUS_PER_NODE}" --block &
     sleep 5
 done
 
 COMMAND="unset ROCR_VISIBLE_DEVICES; \
+export TMPDIR=${RAY_NODE_TMP}; \
 export HF_HOME=/root/tmpdir/hf_home; \
 export PYTHONPATH=/root/ReMA-public/src:/verl:\$PYTHONPATH; \
 export RAY_ADDRESS=${IP_HEAD}; \
@@ -95,4 +106,7 @@ PYTHONUNBUFFERED=1 srun --overlap --nodes=1 --ntasks=1 -w "$HEAD_NODE" \
 
 if [[ -n ${JOB_TMP:-} ]]; then
     rm -rf "$JOB_TMP"/*
+fi
+if [[ -n ${RAY_NODE_TMP:-} ]]; then
+    srun --nodes="${SLURM_NNODES}" --ntasks="${SLURM_NNODES}" bash -lc 'rm -rf "'"${RAY_NODE_TMP}"'"'
 fi
